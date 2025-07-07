98f897d35fac0a601791f9b703d9bad443fc3c2d
RABIUL HASSAN <rabiulhassandev@gmail.com>
2025-07-07 11:39:37 +0800

    uni-app routing

---
+++ src/pages/index/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <view class="container">
+        <view class="content">
+            <text class="title">关于我们</text>
+            <text class="description">这是关于我们页面的内容。</text>
+        </view>
+    </view>
+</template>
